5996b9ecb52076f1136e4f18f2a55ec871dc6a42
Alex Sergeyev <alex.sergeyev@gmail.com>
2022-09-08 14:16:00 +0200

    Create an S3 bucket and block public access

--- website.tf
+++ website.tf
@@ -12,3 +12,26 @@ terraform {
     }
   }
 }
+
+locals {
+  s3_bucket = "hiq-workshop"
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket        = local.s3_bucket
+  force_destroy = true
+}
+
+# Block all public access
+resource "aws_s3_bucket_acl" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.bucket
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_block" {
+  bucket                  = aws_s3_bucket.s3_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}

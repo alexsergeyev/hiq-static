3bd76e279dce9c36b22abdcc8ba9784ffd852d38
Alex Sergeyev <alex.sergeyev@gmail.com>
2022-09-08 21:52:10 +0200

    Add CloudFront permissions to the S3 bucket

--- website.tf
+++ website.tf
@@ -49,3 +49,27 @@ resource "aws_s3_bucket_public_access_block" "s3_block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+
+# Allow cloudfront access by OIC
+resource "aws_s3_bucket_policy" "cloudfront" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  policy = data.aws_iam_policy_document.cloudfront.json
+}
+
+data "aws_iam_policy_document" "cloudfront" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.s3_bucket.arn}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+  }
+}
